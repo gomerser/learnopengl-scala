ddd12c1d9396f4b9395be1463c23a8d67a2fdedd
Erik Gomersbach <erik@gomersba.ch>
2025-12-10 22:06:36 +0100

    added chapter 5.3.1.3

---
+++ src/main/scala/5.advanced_lighting/3.1.3.shadow_mapping/shadow_mapping.scala
@@ -0,0 +1,569 @@
+package learnopengl_5_3_1_3
+
+import learnopengl.Camera
+import learnopengl.CameraMovement.*
+import learnopengl.shader_m.Shader
+import org.joml.Matrix4f
+import org.joml.Vector3f
+import org.lwjgl.BufferUtils
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.glfw.GLFWErrorCallback
+import org.lwjgl.opengl.GL
+import org.lwjgl.opengl.GL11.*
+import org.lwjgl.opengl.GL12.*
+import org.lwjgl.opengl.GL13.*
+import org.lwjgl.opengl.GL15.*
+import org.lwjgl.opengl.GL20.*
+import org.lwjgl.opengl.GL21.*
+import org.lwjgl.opengl.GL30.*
+import org.lwjgl.opengl.GL31.*
+import org.lwjgl.opengl.GL33.*
+import org.lwjgl.stb.STBImage.*
+import org.lwjgl.system.Configuration
+import org.lwjgl.system.MemoryStack
+import org.lwjgl.system.MemoryUtil
+
+import java.io.InputStream
+import java.nio.ByteBuffer
+
+// settings
+var SCR_WIDTH = 800
+var SCR_HEIGHT = 600
+
+// camera
+val camera = Camera(Vector3f(0.0f, 0.0f, 3.0f))
+var lastX = SCR_WIDTH / 2.0f
+var lastY = SCR_HEIGHT / 2.0f
+var firstMouse: Boolean = true
+
+// timing
+var deltaTime: Float = 0.0f
+var lastFrame: Float = 0.0f
+
+// plane VAO
+var planeVAO = 0
+
+@main def main(): Unit =
+
+  // glfw window creation
+  // --------------------
+  if (!glfwInit()) throw new IllegalStateException("Unable to initialize GLFW")
+  GLFWErrorCallback.createPrint(System.err).set()
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3)
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)
+
+  if (System.getProperty("os.name").toLowerCase.contains("mac"))
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE)
+
+  // glfw window creation
+  // --------------------
+  val window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", 0, 0)
+  if (window == 0L) {
+    glfwTerminate()
+    throw new RuntimeException("Failed to create GLFW window")
+  }
+  glfwMakeContextCurrent(window)
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback)
+  glfwSetCursorPosCallback(window, mouse_callback)
+  glfwSetScrollCallback(window, scroll_callback)
+
+  // tell GLFW to capture our mouse
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
+
+  // fetch framebuffer width and height
+  val (fbWidth, fbHeight) = usingStack { stack =>
+    val xBuf = stack.mallocInt(1)
+    val yBuf = stack.mallocInt(1)
+    glfwGetFramebufferSize(window, xBuf, yBuf)
+    (xBuf.get(0), yBuf.get(0))
+  }
+
+  // load all OpenGL function pointers for the current context — it’s the LWJGL equivalent of gladLoadGLLoader
+  GL.createCapabilities()
+
+  // configure global opengl state
+  // -----------------------------
+  glEnable(GL_DEPTH_TEST)
+
+  // build and compile shaders
+  // -------------------------
+  val shader = Shader(
+    "5.advanced_lighting/3.1.3.shadow_mapping.vs",
+    "5.advanced_lighting/3.1.3.shadow_mapping.fs"
+  )
+  val simpleDepthShader = Shader(
+    "5.advanced_lighting/3.1.3.shadow_mapping_depth.vs",
+    "5.advanced_lighting/3.1.3.shadow_mapping_depth.fs"
+  )
+  val debugDepthQuad = Shader(
+    "5.advanced_lighting/3.1.3.debug_quad.vs",
+    "5.advanced_lighting/3.1.3.debug_quad_depth.fs"
+  )
+
+  // set up vertex data (and buffer(s)) and configure vertex attributes
+  // ------------------------------------------------------------------
+  val planeVertices = Array[Float](
+    // positions            // normals         // texcoords
+    25.0f, -0.5f, 25.0f, 0.0f, 1.0f, 0.0f, 25.0f, 0.0f, -25.0f, -0.5f, 25.0f,
+    0.0f, 1.0f, 0.0f, 0.0f, 0.0f, -25.0f, -0.5f, -25.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+    25.0f, 25.0f, -0.5f, 25.0f, 0.0f, 1.0f, 0.0f, 25.0f, 0.0f, -25.0f, -0.5f,
+    -25.0f, 0.0f, 1.0f, 0.0f, 0.0f, 25.0f, 25.0f, -0.5f, -25.0f, 0.0f, 1.0f,
+    0.0f, 25.0f, 25.0f
+  )
+  // plane VAO
+  planeVAO = glGenVertexArrays()
+  val planeVBO = glGenBuffers()
+  glBindVertexArray(planeVAO)
+  glBindBuffer(GL_ARRAY_BUFFER, planeVBO)
+  val planeBuf =
+    MemoryUtil.memAllocFloat(planeVertices.length).put(planeVertices).flip()
+  glBufferData(GL_ARRAY_BUFFER, planeBuf, GL_STATIC_DRAW)
+  MemoryUtil.memFree(planeBuf)
+  glEnableVertexAttribArray(0)
+  glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * java.lang.Float.BYTES, 0)
+  glEnableVertexAttribArray(1)
+  glVertexAttribPointer(
+    1,
+    3,
+    GL_FLOAT,
+    false,
+    8 * java.lang.Float.BYTES,
+    3 * java.lang.Float.BYTES
+  )
+  glEnableVertexAttribArray(2)
+  glVertexAttribPointer(
+    2,
+    2,
+    GL_FLOAT,
+    false,
+    8 * java.lang.Float.BYTES,
+    6 * java.lang.Float.BYTES
+  )
+  glBindVertexArray(0)
+
+  // load textures
+  // -------------
+  val woodTexture = loadTexture("/textures/wood.png")
+
+  // configure depth map FBO
+  // -----------------------
+  val SHADOW_WIDTH = 1024
+  val SHADOW_HEIGHT = 1024
+  val depthMapFBO = glGenFramebuffers()
+  // create depth texture
+  val depthMap = glGenTextures()
+  glBindTexture(GL_TEXTURE_2D, depthMap)
+  glTexImage2D(
+    GL_TEXTURE_2D,
+    0,
+    GL_DEPTH_COMPONENT,
+    SHADOW_WIDTH,
+    SHADOW_HEIGHT,
+    0,
+    GL_DEPTH_COMPONENT,
+    GL_FLOAT,
+    0L
+  )
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER)
+  val borderColor = Array(1.0f, 1.0f, 1.0f, 1.0f)
+  glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor)
+  // attach depth texture as FBO's depth buffer
+  glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO)
+  glFramebufferTexture2D(
+    GL_FRAMEBUFFER,
+    GL_DEPTH_ATTACHMENT,
+    GL_TEXTURE_2D,
+    depthMap,
+    0
+  )
+  glDrawBuffer(GL_NONE)
+  glReadBuffer(GL_NONE)
+  glBindFramebuffer(GL_FRAMEBUFFER, 0)
+
+  // shader configuration
+  // --------------------
+  shader.use()
+  shader.setInt("diffuseTexture", 0)
+  shader.setInt("shadowMap", 1)
+  debugDepthQuad.use()
+  debugDepthQuad.setInt("depthMap", 0)
+
+  // lighting info
+  // -------------
+  val lightPos = Vector3f(-2.0f, 4.0f, -1.0f)
+
+  // render loop
+  // -----------
+  while (!glfwWindowShouldClose(window)) {
+
+    // per-frame time logic
+    // --------------------
+    val currentFrame = glfwGetTime().toFloat
+    deltaTime = currentFrame - lastFrame
+    lastFrame = currentFrame
+
+    // input
+    // -----
+    processInput(window)
+
+    // change light position over time
+    // lightPos.x = math.sin(glfwGetTime).toFloat * 3.0f
+    // lightPos.z = math.cos(glfwGetTime).toFloat * 2.0f
+    // lightPos.y = 5.0f + math.cos(glfwGetTime).toFloat * 1.0f
+
+    // render
+    // ------
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    // 1. render depth of scene to texture (from light's perspective)
+    // --------------------------------------------------------------
+    val near_plane = 1f
+    val far_plane = 7.5f
+    // val lightProjection = Matrix4f().perspective(math.toRadians(45.0f).toFloat, SHADOW_WIDTH.toFloat / SHADOW_HEIGHT.toFloat, near_plane, far_plane) // note that if you use a perspective projection matrix you'll have to change the light position as the current light position isn't enough to reflect the whole scene
+    val lightProjection =
+      Matrix4f().ortho(-10f, 10f, -10f, 10f, near_plane, far_plane)
+    val lightView =
+      Matrix4f().lookAt(lightPos, Vector3f(0.0f), Vector3f(0.0f, 1.0f, 0.0f))
+    val lightSpaceMatrix = Matrix4f(lightProjection).mul(lightView)
+    // render scene from light's point of view
+    simpleDepthShader.use()
+    simpleDepthShader.setMat4("lightSpaceMatrix", lightSpaceMatrix)
+
+    glViewport(0, 0, SHADOW_WIDTH, SHADOW_HEIGHT)
+    glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO)
+    glClear(GL_DEPTH_BUFFER_BIT)
+    glActiveTexture(GL_TEXTURE0)
+    glBindTexture(GL_TEXTURE_2D, woodTexture)
+    renderScene(simpleDepthShader)
+    glBindFramebuffer(GL_FRAMEBUFFER, 0)
+
+    // reset viewport
+    glViewport(0, 0, fbWidth, fbHeight)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+    // 2. render scene as normal using the generated depth/shadow map
+    // --------------------------------------------------------------
+    shader.use()
+    val projection = Matrix4f()
+      .identity()
+      .perspective(
+        Math.toRadians(camera.zoom).toFloat,
+        SCR_WIDTH.toFloat / SCR_HEIGHT.toFloat,
+        0.1f,
+        100.0f
+      )
+    val view = camera.getViewMatrix
+    shader.setMat4("projection", projection)
+    shader.setMat4("view", view)
+    // set light uniforms
+    shader.setVec3("viewPos", camera.position)
+    shader.setVec3("lightPos", lightPos)
+    shader.setMat4("lightSpaceMatrix", lightSpaceMatrix)
+    glActiveTexture(GL_TEXTURE0)
+    glBindTexture(GL_TEXTURE_2D, woodTexture)
+    glActiveTexture(GL_TEXTURE1)
+    glBindTexture(GL_TEXTURE_2D, depthMap)
+    renderScene(shader)
+
+    // render Depth map to quad for visual debugging
+    // ---------------------------------------------
+    debugDepthQuad.use()
+    debugDepthQuad.setFloat("near_plane", near_plane)
+    debugDepthQuad.setFloat("far_plane", far_plane)
+    glActiveTexture(GL_TEXTURE0)
+    glBindTexture(GL_TEXTURE_2D, depthMap)
+    // renderQuad()
+
+    // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+    // -------------------------------------------------------------------------------
+    glfwSwapBuffers(window)
+    glfwPollEvents()
+  }
+
+  // optional: de-allocate all resources once they've outlived their purpose:
+  // ------------------------------------------------------------------------
+  glDeleteVertexArrays(planeVAO)
+  glDeleteBuffers(planeVBO)
+
+  glfwTerminate()
+
+// renders the 3D scene
+// --------------------
+def renderScene(shader: Shader): Unit =
+  // floor
+  val model = Matrix4f()
+  shader.setMat4("model", model)
+  glBindVertexArray(planeVAO)
+  glDrawArrays(GL_TRIANGLES, 0, 6)
+  // cubes
+  val m1 = Matrix4f()
+    .translate(0f, 1.5f, 0.0)
+    .scale(0.5f)
+  shader.setMat4("model", m1)
+  renderCube()
+  val m2 = Matrix4f()
+    .translate(2.0f, 0.0f, 1.0)
+    .scale(0.5f)
+  shader.setMat4("model", m2)
+  renderCube()
+  val m3 = Matrix4f()
+    .translate(-1.0f, 0.0f, 2.0)
+    .rotate(Math.toRadians(60.0f).toFloat, Vector3f(1.0, 0.0, 1.0).normalize())
+    .scale(0.25f)
+  shader.setMat4("model", m3)
+  renderCube()
+
+// renderCube() renders a 1x1 3D cube in NDC.
+// -------------------------------------------------
+var cubeVAO = 0
+var cubeVBO = 0
+def renderCube(): Unit =
+  // initialize (if necessary)
+  if cubeVAO == 0 then
+    val vertices = Array[Float](
+      // back face
+      -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+      1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f, // top-right
+      1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 0.0f, // bottom-right
+      1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 1.0f, 1.0f, // top-right
+      -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, // bottom-left
+      -1.0f, 1.0f, -1.0f, 0.0f, 0.0f, -1.0f, 0.0f, 1.0f, // top-left
+      // front face
+      -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-left
+      1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, // bottom-right
+      1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // top-right
+      1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // top-right
+      -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, // top-left
+      -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom-left
+      // left face
+      -1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-right
+      -1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top-left
+      -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-left
+      -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-left
+      -1.0f, -1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom-right
+      -1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-right
+      // right face
+      1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-left
+      1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-right
+      1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // top-right
+      1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom-right
+      1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // top-left
+      1.0f, -1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // bottom-left
+      // bottom face
+      -1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, // top-right
+      1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 1.0f, // top-left
+      1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, // bottom-left
+      1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, // bottom-left
+      -1.0f, -1.0f, 1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, // bottom-right
+      -1.0f, -1.0f, -1.0f, 0.0f, -1.0f, 0.0f, 0.0f, 1.0f, // top-right
+      // top face
+      -1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top-left
+      1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom-right
+      1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, // top-right
+      1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // bottom-right
+      -1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, // top-left
+      -1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f // bottom-left
+    )
+
+    cubeVAO = glGenVertexArrays()
+    cubeVBO = glGenBuffers()
+    // fill buffer
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO)
+    val buf = MemoryUtil.memAllocFloat(vertices.length).put(vertices).flip()
+    glBufferData(GL_ARRAY_BUFFER, buf, GL_STATIC_DRAW)
+    MemoryUtil.memFree(buf)
+    // link vertex attributes
+    glBindVertexArray(cubeVAO)
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 8 * java.lang.Float.BYTES, 0)
+    glEnableVertexAttribArray(1)
+    glVertexAttribPointer(
+      1,
+      3,
+      GL_FLOAT,
+      false,
+      8 * java.lang.Float.BYTES,
+      3 * java.lang.Float.BYTES
+    )
+    glEnableVertexAttribArray(2)
+    glVertexAttribPointer(
+      2,
+      2,
+      GL_FLOAT,
+      false,
+      8 * java.lang.Float.BYTES,
+      6 * java.lang.Float.BYTES
+    )
+    glBindBuffer(GL_ARRAY_BUFFER, 0)
+    glBindVertexArray(0)
+
+  // render Cube
+  glBindVertexArray(cubeVAO)
+  glDrawArrays(GL_TRIANGLES, 0, 36)
+  glBindVertexArray(0)
+
+// renderQuad() renders a 1x1 XY quad in NDC
+// -----------------------------------------
+var quadVAO = 0
+var quadVBO = 0
+def renderQuad(): Unit =
+  if quadVAO == 0 then
+    val quadVertices = Array[Float](
+      // positions        // texture Coords
+      -1.0f, 1.0f, 0.0f, 0.0f, 1.0f, -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+      0.0f, 1.0f, 1.0f, 1.0f, -1.0f, 0.0f, 1.0f, 0.0f
+    )
+    // setup plane VAO
+    quadVAO = glGenVertexArrays()
+    quadVBO = glGenBuffers()
+    glBindVertexArray(quadVAO)
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO)
+    val buf =
+      MemoryUtil.memAllocFloat(quadVertices.length).put(quadVertices).flip()
+    glBufferData(GL_ARRAY_BUFFER, buf, GL_STATIC_DRAW)
+    MemoryUtil.memFree(buf)
+    glEnableVertexAttribArray(0)
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, 5 * java.lang.Float.BYTES, 0)
+    glEnableVertexAttribArray(1)
+    glVertexAttribPointer(
+      1,
+      2,
+      GL_FLOAT,
+      false,
+      5 * java.lang.Float.BYTES,
+      3 * java.lang.Float.BYTES
+    )
+
+  glBindVertexArray(quadVAO)
+  glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+  glBindVertexArray(0)
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+def processInput(window: Long): Unit = {
+  if glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS then
+    glfwSetWindowShouldClose(window, true)
+
+  if glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS then
+    camera.processKeyboard(FORWARD, deltaTime)
+  if glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS then
+    camera.processKeyboard(BACKWARD, deltaTime)
+  if glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS then
+    camera.processKeyboard(LEFT, deltaTime)
+  if glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS then
+    camera.processKeyboard(RIGHT, deltaTime)
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+def framebuffer_size_callback(window: Long, width: Int, height: Int): Unit = {
+  // make sure the viewport matches the new window dimensions; note that width and
+  // height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height)
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+def mouse_callback(window: Long, xposIn: Double, yposIn: Double): Unit = {
+  val xpos = xposIn.toFloat
+  val ypos = yposIn.toFloat
+
+  if (firstMouse) {
+    lastX = xpos
+    lastY = ypos
+    firstMouse = false
+  }
+
+  var xoffset = xpos - lastX
+  var yoffset =
+    lastY - ypos // reversed since y-coordinates go from bottom to top
+  lastX = xpos
+  lastY = ypos
+
+  camera.processMouseMovement(xoffset, yoffset)
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+def scroll_callback(window: Long, xoffset: Double, yoffset: Double): Unit = {
+  camera.processMouseScroll(yoffset.toFloat)
+}
+
+// utility function for loading a 2D texture from file
+// ---------------------------------------------------
+def loadTexture(path: String): Int = {
+  val textureID = glGenTextures()
+
+  usingStack { stack =>
+    val w = stack.mallocInt(1)
+    val h = stack.mallocInt(1)
+    val nrComponents = stack.mallocInt(1)
+
+    val imgBytes = loadResourceAsTexture(path)
+    val data = stbi_load_from_memory(imgBytes, w, h, nrComponents, 0)
+    if (data == null)
+      throw new RuntimeException("Texture failed to load at path: " + path)
+
+    val format = nrComponents.get() match {
+      case 1 => GL_RED
+      case 3 => GL_RGB
+      case 4 => GL_RGBA
+    }
+
+    glBindTexture(GL_TEXTURE_2D, textureID)
+    glTexImage2D(
+      GL_TEXTURE_2D,
+      0,
+      format,
+      w.get(),
+      h.get(),
+      0,
+      format,
+      GL_UNSIGNED_BYTE,
+      data
+    )
+    glGenerateMipmap(GL_TEXTURE_2D)
+
+    glTexParameteri(
+      GL_TEXTURE_2D,
+      GL_TEXTURE_WRAP_S,
+      if (format == GL_RGBA) GL_CLAMP_TO_EDGE else GL_REPEAT
+    )
+    glTexParameteri(
+      GL_TEXTURE_2D,
+      GL_TEXTURE_WRAP_T,
+      if (format == GL_RGBA) GL_CLAMP_TO_EDGE else GL_REPEAT
+    )
+    glTexParameteri(
+      GL_TEXTURE_2D,
+      GL_TEXTURE_MIN_FILTER,
+      GL_LINEAR_MIPMAP_LINEAR
+    )
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+
+    stbi_image_free(data)
+  }
+
+  textureID
+}
+
+def usingStack[A](f: MemoryStack => A): A =
+  val stack = MemoryStack.stackPush()
+  try f(stack)
+  finally stack.pop()
+
+def loadResourceAsTexture(name: String): ByteBuffer =
+  val stream = this.getClass.getResourceAsStream(name)
+  if stream == null then
+    throw new RuntimeException("Resource not found: " + name)
+
+  val bytes = stream.readAllBytes()
+  val buffer = BufferUtils.createByteBuffer(bytes.length)
+  buffer.put(bytes)
+  buffer.flip()
+  buffer
